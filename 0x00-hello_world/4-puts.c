a4df226550da7dd24dff192a50325c789f8c369e
Kayode <sirkay1001@gmail.com>
2022-09-08 13:16:23 -0700

    add a C program that prints a sentnce using the puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+
 /**
- * main - Prints out the sentence programming is like building a multilingual puzzle 
- * using the function puts
+ * main - Prints out the sentence programming is like building a multilingual puzzle using the function puts
  * Return: 0 (exited succesfully)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
