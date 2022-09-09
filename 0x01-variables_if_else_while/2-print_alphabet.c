a4c484b11a6415d3d11a3a20a4aea57c77f883bb
Kayode <sirkay1001@gmail.com>
2022-09-09 12:04:03 -0700

    print the letters of the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints out whether a number randomly generated
+ * is positive, negetive or zero
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int alphabet = 97;
+
+	for (int i = 0; i < 26; i++)
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	return (0);
+}	
