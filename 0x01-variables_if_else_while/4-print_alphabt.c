d2aee49304e21c4c2c128caf5e489d32c310d4d1
Kayode <sirkay1001@gmail.com>
2022-09-09 12:56:32 -0700

    print the alphabets but skip q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - prints out whether a number randomly generated
+ * is positive, negetive or zero
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	int alphabet = 97;
+
+	for (i = 0; i < 26; i++)
+	{
+		if(alphabet == 101 || alphabet == 113)
+		{
+			alphabet++;
+			continue;
+		}
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
+
