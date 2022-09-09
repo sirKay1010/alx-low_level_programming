ec0d32717276e35d6ad576967e89b52c70e22da3
Kayode <sirkay1001@gmail.com>
2022-09-09 10:14:22 -0700

    print whether a number is positive. negetive or zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,6 +5,7 @@
 /**
  * main - prints out whether a number randomly generated
  * is positive, negetive or zero
+ * return: Always 0 (success)
  */
 
 int main(void)
@@ -12,12 +13,12 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n  = rand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n == 0)
 		printf("%d is zero\n", n);
 	else if (n > 0)
 	  	printf("%d is positive\n", n);
-	else if (n < 0)
+	else
 		printf("%d is negetive\n", n);
 	return (0);
 }
