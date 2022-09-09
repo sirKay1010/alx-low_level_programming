50394ae55acfd4006420178ac037ae23790d858e
Kayode <sirkay1001@gmail.com>
2022-09-09 14:33:33 -0700

    print alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - prints out whether a number randomly generated
- * is positive, negetive or zero
+ * main - prints out alphabets
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	int alphabet = 97;
+	int alphabet = 65;
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i > 26; i++)
 	{
 		putchar(alphabet);
 		alphabet++;
 	}
 	putchar('\n');
 	return (0);
-}	
+}
