cb0673916f062d4f7fc92ff5a67e43b5c9aa522a
Kayode <sirkay1001@gmail.com>
2022-09-09 11:34:33 -0700

    print the last digit of a number and determine is positive, negetive or 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,24 +3,23 @@
 #include <time.h>
 
 /**
- * main - prints out whether a number randomly generated
- * is positive, negetive or zero
- * return: Always 0 (success)
+ * main - prints what the last digit is and whether its negetive positive or 0
+ * Return: 0 if succesful
  */
 
 int main(void)
 {
 	int n;
 	int m;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	m = n % 10;
+	m  = n % 10;
 	if (m == 0)
 		printf("Last digit of %d is %d and is 0\n", n, m);
-	else if (m > 5)
+	else if (n > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, m);
 	else if (n < 6 && n != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, m);
 	return (0);
 }
+
