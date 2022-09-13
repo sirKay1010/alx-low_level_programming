c242167e231eb927dffb07670f5ebeb3d6c8ab4b
Kayode <sirkay1001@gmail.com>
2022-09-13 04:40:35 -0700

    print _putchar to the screen

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out _putchar to the screen
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
