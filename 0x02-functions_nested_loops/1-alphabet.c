3f8955ca892c461abf2bb3c18ca1ba860eec19ce
Kayode <sirkay1001@gmail.com>
2022-09-13 05:07:42 -0700

    print out the alphabets using a function defined in main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints out the alphabets using a function
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	putchar('\n');
+	return (0);
+}
