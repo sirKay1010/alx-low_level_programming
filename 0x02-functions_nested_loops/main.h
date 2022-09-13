8b8d279640dd6cab9cca455a8acfacb82d563b49
Kayode <sirkay1001@gmail.com>
2022-09-13 15:15:33 -0700

    compute the absolute value of an int

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,3 +4,8 @@ void print_alphabet_x10(void);
 int _islower(int a);
 int _isalpha(int a);
 int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+
