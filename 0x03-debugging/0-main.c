8a672c4aa4417a86be0d3ac942c28b22354cefc8
Kayode <sirkay1001@gmail.com>
2022-09-15 02:55:24 -0700

    check if a functions output is correct

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - a c file to test the output of the function
+ * positive_or_negetive
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int digit = 0;
+
+	positive_or_negative(digit);
+
+	return (0);
+}
