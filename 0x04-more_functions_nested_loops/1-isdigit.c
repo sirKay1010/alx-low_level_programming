ee1b8a69482eec3fe70b7095efd67e57e69bfcab
Kayode <sirkay1001@gmail.com>
2022-09-15 09:07:38 -0700

    check for a digit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -6,9 +6,9 @@
  * Return: 1 if c is digit, 0 otherwise
  */
 
-int _isdigit(int c);
+int _isdigit(int c)
 {
-	if ((c >= 48 && c < 58)|| (c >= 0 && c <= 9))
+	if (c >= 48 && c < 58)
 	{
 		return (1);
 	}
