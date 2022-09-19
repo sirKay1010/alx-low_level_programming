cd02073869d545c4c7bdcd83223bd977311a3b3b
Kayode <sirkay1001@gmail.com>
2022-09-19 16:40:53 -0700

    copy a string and print it

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcpy - prints a string to stduout
+ * @dest: buffer pointer char
+ * @src: char * pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return (dest);
+}
