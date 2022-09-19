d694b6d1b49e54b4a6b84c5586355c43706d6bf6
Kayode <sirkay1001@gmail.com>
2022-09-19 15:54:26 -0700

    print half of a string from middle to end

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * puts_half - prints a string to stduout
+ * @str: pointer char
+ */
+
+void puts_half(char *str)
+{
+	char *p;
+	char c;
+	int len, n;
+
+	p = str;
+	len = 0;
+	while (*p != 0)
+	{
+		p = p + 1;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+		str = str + n;
+		/*
+		 * for (i = n; i < len - 1; i++)
+		 *{
+		 *	_putchar(*str);
+		 *	str++;
+		 *}
+		 */
+		while (*str != 0)
+		{
+			c = *str;
+			_putchar(c);
+			str++;
+		}
+		_putchar('\n');
+	}
+}
