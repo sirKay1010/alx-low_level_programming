e96bed68bf2864262a6e9b5f99f6e022778b2888
Kayode <sirkay1001@gmail.com>
2022-09-19 14:43:53 -0700

    print every other character in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other digit in a string
+ * @str: pointer char
+ */
+
+void puts2(char *str)
+{
+	char c;
+
+	/**
+	 * for (c = *str; c != 0; str = str + 2)
+	 *{
+	 *	_putchar(c);
+	 *}
+	 *_putchar('\n');
+	 */
+	while (*str != 0)
+	{
+		c = *str;
+		_putchar(c);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
