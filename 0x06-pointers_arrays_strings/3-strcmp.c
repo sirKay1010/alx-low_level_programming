a22a89dc53f5c743b961d8357487e3e90df016c5
Kayode <sirkay1001@gmail.com>
2022-09-21 13:38:19 -0700

    compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -16,29 +16,26 @@ int _strcmp(char *s1, char *s2)
 
 	if (*s1 == 0 && *s2 == 0)
 		return (result);
-	else if (*s1 == 0 && *s2 != 0)
-		return (result - 1);
-	else if (*s1 != 0 && *s2 == 0)
+	if (*s1 == 0 && *s2 != 0)
 		return (result + 1);
-	else
+	if (*s1 != 0 && *s2 == 0)
+		return (result - 1);
+	while (*s1 != 0 && *s2 != 0)
 	{
-		while (*s1 != 0 && *s2 != 0)
+		if (*s1 < *s2)
 		{
-			if (*s1 < *s2)
-			{
-				result--;
-				return (result);
-			}
-			else if (*s1 > *s2)
-			{
-				result++;
-				return (result);
-			}
-			else
-			{
-				s1++;
-				s2++;
-			}
+			result--;
+			return (result);
+		}
+		else if (*s1 > *s2)
+		{
+			result++;
+			return (result);
+		}
+		else
+		{
+			s1++;
+			s2++;
 		}
 	}
 	return (result);
