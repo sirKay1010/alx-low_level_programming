132eb2f6944334c3bb58fd25814d13046f5ce471
Kayode <sirkay1001@gmail.com>
2022-09-21 14:16:28 -0700

    reverse an array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: pointer to array
+ * @n: the number of bytes to be used
+ *
+ * Return: the pointer to dest
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int count = 1;
+	int *p;
+	int stop_count;
+
+	p = a;
+	while (count < n)
+	{
+		p++;
+		count++;
+	}
+	count = 0;
+	stop_count = n/2;
+	while (count < (stop_count - 1))
+	{
+		temp = *a;
+		*a = *p;
+		*p = temp;
+		a++;
+		p--;
+		count++;
+	}
+}
