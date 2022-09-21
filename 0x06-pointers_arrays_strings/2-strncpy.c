60f44fd9ac37071835e72d4b5e13fab589777d14
Kayode <sirkay1001@gmail.com>
2022-09-21 12:36:53 -0700

    copy a string to another n bytes number of times

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - copies the string pointed to by src
+ * with at most n bytes
+ * to the buffer pointed to by dest
+ * @dest: pointer to the buffer in which we copy the string
+ * @src: string to be copied
+ * @n: the number of bytes to be used
+ * Return: the pointer to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int count = 0;
+	char *temp;
+
+	temp = dest;
+	while (count < n)
+	{
+		if (*src == 0)
+		{
+			*dest = 0;
+		}
+		else
+		{
+			*dest = *src;
+			dest++;
+			src++;
+		}
+		count++;
+	}
+	return (temp);
+}
