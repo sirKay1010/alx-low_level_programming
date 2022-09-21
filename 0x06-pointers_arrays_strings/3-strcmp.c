64184be4127ee8214058f394fb52d745713bc947
Kayode <sirkay1001@gmail.com>
2022-09-21 13:12:29 -0700

    compare two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strcmp - copies the string pointed to by src
+ * with at most n bytes
+ * to the buffer pointed to by dest
+ * @s1: pointer to the buffer in which we copy the string
+ * @s2: string to be copied
+ *
+ * Return: -1 0 or 1 depending on if lessthan equal or greaterthan
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = 0;
+
+	while (*s1 != 0 && * s2 != 0)
+	{
+		if (*s1 < *s2)
+		{
+			result--;
+			return (result);
+		}
+		else if (*s1 > *s2)
+		{
+			result++;
+			return (result);
+		}
+		else
+		{
+			s1++;
+			s2++;
+		}
+	}
+	return (result);
+}
