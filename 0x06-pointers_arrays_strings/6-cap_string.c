687bc1641c533f2ab2ebba279d798e41f048aedb
Kayode <sirkay1001@gmail.com>
2022-09-22 01:15:00 -0700

    encode a string using into 1337

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,4 +8,26 @@
 
 char *cap_string(char *str)
 {
+	char *temp;
+
+	temp = str;
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i = 0)
+		{
+			if (str[i] >= 65 && str[i] <= 90)
+			{
+				str[i] += 32;
+			}
+		}
+		if (*temp == 32 || *temp == '\n' || temp == '\t')
+		{
+			temp++;
+			if (*temp >= 65 && *temp <= 90)
+			{
+				*temp = *temp + 32;
+
+			}
+		}
+	}
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,9 +4,36 @@
  * leet - encodes a string into 1337
  * @str: pointer to string
  * Return: returns pointer str
- * */
+ */
 
 char *leet(char *str)
 {
+	char *tmp;
 
+	tmp = str;
+	while (*tmp != 0)
+	{
+		if (*tmp == 'a' || *tmp == 'A')
+		{
+			*tmp = 52;
+		}
+		else if (*tmp == 'e' || *tmp == 'E')
+		{
+			*tmp = 51;
+		}
+		else if (*tmp == 'o' || *tmp == 'O')
+		{
+			*tmp = 48;
+		}
+		else if (*tmp == 't' || *tmp == 'T')
+		{
+			*tmp = 55;
+		}
+		else if (*tmp == 'l' || *tmp == 'L')
+		{
+			*tmp = 49;
+		}
+		tmp++;
+	}
+	return (str);
 }
