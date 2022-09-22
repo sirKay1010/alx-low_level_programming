dc937916db48e2b88ae0c4625a55149946040071
Kayode <sirkay1001@gmail.com>
2022-09-22 13:39:59 -0700

    capitalize each word in a sentence

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,21 +10,21 @@ char *cap_string(char *str)
 {
 	int i, j;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.',
+	char sp[13] = {' ', '\t', '\n', ',', ';', '.',
 		'!', '?', '"', '(', ')', '{', '}'};
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
+		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 
 		for (j = 0; j < 13; j++)
 		{
-			if (s[i] == spe[j])
+			if (str[i] == sp[j])
 			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 				{
-					s[i + 1] -= 32;
+					str[i + 1] -= 32;
 				}
 			}
 		}
