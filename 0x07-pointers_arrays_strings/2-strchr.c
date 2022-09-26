d77ae2a38749af27d8ea53f001d93836b6d35342
Kayode <sirkay1001@gmail.com>
2022-09-26 15:04:33 -0700

    get length of a prefix substring

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,22 +1,28 @@
 #include "main.h"
-/**
- * _strchr - checks if a character exists in a string
- * @s: the string
- * @c: the character
- * Return: c is the character is present, otherwise NULL
- */
+#include <stdio.h>
 
+/**
+ * *_strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
 char *_strchr(char *s, char c)
 {
-	int i;
+		int a;
 
-	i = 0;
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-
-	return (NULL);
+		while (1)
+		{
+			a = *s++;
+			if (a == c)
+			{
+				return (s - 1);
+			}
+			if (a == 0)
+			{
+				return (NULL);
+			}
+		}
 }
