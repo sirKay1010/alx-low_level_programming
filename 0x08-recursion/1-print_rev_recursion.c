4268ad03d1509342ad48120b6f818f06d2bb5be4
Kayode <sirkay1001@gmail.com>
2022-09-29 02:56:57 -0700

    print a string in reverse using recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _print_rev_recusion - prints a string in reverse using recursion.
- * @s - pointer to string
+ * _print_rev_recursion - prints a string in reverse using recursion.
+ * @s: pointer to string
  */
 
 void _print_rev_recursion(char *s)
